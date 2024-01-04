5d94fb64c34cee6016d631b2825ba95dc2d83fa1
Rushi9867 <rushikhandare2203@gmail.com>
2024-01-04 19:01:24 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 REPO_NAME = "MLOPS_MongoDB_Demo"
 PKG_NAME= "mongodbconnection"
 AUTHOR_USER_NAME = "Rushi9867"
